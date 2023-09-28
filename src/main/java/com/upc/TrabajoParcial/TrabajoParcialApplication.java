ba2b53b18e33f57b94669e4a106fa378f8d6b380
Martin HQ <u201614196@upc.edu.pe>
2023-09-27 19:11:48 -0500

    agregue carpetas base

---
+++ src/main/java/com/upc/TrabajoParcial/TrabajoParcialApplication.java
@@ -0,0 +1,13 @@
+package com.upc.TrabajoParcial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoParcialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoParcialApplication.class, args);
+	}
+
+}
